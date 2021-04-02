4e26ce49f0710a0e7e1ff3b4df2ceceb5b763d40
Seth Lewandowski <sethlew@protonmail.com>
2021-04-02 12:15:16 -0700

    finished npm overview

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,15 +1,1 @@
-// http module basics
 
-const http = require('http')
-
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.end('welcome to home page')
-  } else if (req.url === '/about') {
-    res.end('here is our short history')
-  } else {
-    res.end(`<h1>Oops! This page doesn't exist</h1>`)
-  }
-})
-
-server.listen(5000)
