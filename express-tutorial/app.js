12a0229767be722d59f4f4aadec496db633e11ad
Seth Lewandowski <sethlew@protonmail.com>
2021-04-02 15:28:33 -0700

    end naked http module examples... express now

--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -1,8 +1,24 @@
 const http = require('http')
+const { readFileSync, readFile } = require('fs')
+
+// get all files
+const homePage = readFileSync('./navbar-app/index.html')
 
 const server = http.createServer((req, res) => {
-  console.log('user hit the server')
-  res.end('home page')
+  const url = req.url
+  if (url === '/') {
+    res.writeHead(200, { 'content-type': 'text/html' })
+    res.write(homePage)
+    res.end()
+  } else if (url === '/about') {
+    res.writeHead(200, { 'content-type': 'text/html' })
+    res.write('<h1>About Page</h1>')
+    res.end()
+  } else {
+    res.writeHead(404, { 'content-type': 'text/html' })
+    res.write('<h1>Page Not found</h1>')
+    res.end()
+  }
 })
 
 server.listen(5000)
