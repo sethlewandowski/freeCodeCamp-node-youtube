7061706464b5208f014f5bc609b88813c7712183
Seth Lewandowski <sethlew@protonmail.com>
2021-04-03 15:44:50 -0700

    added req.query functionality

--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -32,7 +32,19 @@ app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
 
 app.get('/api/v1/query', (req, res) => {
   console.log(req.query)
-  res.json('hello world')
+  const { search, limit } = req.query
+  let sortedProducts = [...products]
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search)
+    })
+  }
+
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit))
+  }
+  res.status(200).json(sortedProducts)
 })
 
 app.listen(5000, () => {
