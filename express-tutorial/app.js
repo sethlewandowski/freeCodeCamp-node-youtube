361b2d3898940893d7a2b0282b24b8c4a339f1ad
Seth Lewandowski <sethlew@protonmail.com>
2021-04-03 15:28:29 -0700

    adding two req params to theexample

--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -7,7 +7,27 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/products', (req, res) => {
-  res.json(products)
+  const newProducts = products.map((item) => {
+    const { id, name, image } = item
+    return { id, name, image }
+  })
+  res.json(newProducts)
+})
+
+app.get('/api/products/:id', (req, res) => {
+  const { id } = req.params
+
+  const singleProduct = products.find((product) => product.id === Number(id))
+  if (!singleProduct) {
+    res.status(404).send('Product does not exist!')
+  } else {
+    res.json(singleProduct)
+  }
+})
+
+app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
+  console.log(req.params)
+  res.send(req.params)
 })
 
 app.listen(5000, () => {
